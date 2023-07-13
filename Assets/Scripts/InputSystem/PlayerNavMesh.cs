056b53da4ea8464555051f229887441e2923332d
Afsal Baaqir A <afsalbaaqir@gmail.com>
2023-07-13 19:37:31 +0530

    Enemy Nav

---
+++ Assets/Scripts/InputSystem/PlayerNavMesh.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavMesh : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _navMeshAgent.destination = _target.position;
+    }
+}
